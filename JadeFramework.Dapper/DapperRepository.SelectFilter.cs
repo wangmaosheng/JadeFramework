4da3a66be40eae53cc0559ee2bc1fb8b80dc61d0
wangms <263626005@qq.com>
2019-03-14 21:18:08 +0800

    add repository filter

---
+++ JadeFramework.Dapper/DapperRepository.SelectFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JadeFramework.Dapper
+{
+    /// <summary>
+    /// 查询仓储过滤 =>用于数据筛选
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public partial class DapperRepository<TEntity> where TEntity : class
+    {
+
+    }
+}
