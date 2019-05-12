71a438c5d6b29fb06fe3f68bb4eb0aad7ced0895
wangms <263626005@qq.com>
2019-05-12 10:57:13 +0800

    定制流程修改

---
+++ JadeFramework.WorkFlow/SystemFlowDto.cs
@@ -0,0 +1,17 @@
+﻿namespace JadeFramework.WorkFlow
+{
+    /// <summary>
+    /// 定制流程获取工作流进程DTO
+    /// </summary>
+    public class SystemFlowDto
+    {
+        /// <summary>
+        /// 表单ID
+        /// </summary>
+        public string PageId { get; set; }
+        /// <summary>
+        /// 表单地址
+        /// </summary>
+        public string FormUrl { get; set; }
+    }
+}
