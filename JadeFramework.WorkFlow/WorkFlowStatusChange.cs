a82d86bb68612313a0d8fc797cde7097b9f60bdf
wangms <263626005@qq.com>
2019-05-14 13:46:19 +0800

    优化

---
+++ JadeFramework.WorkFlow/WorkFlowStatusChange.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace JadeFramework.WorkFlow
+{
+    /// <summary>
+    /// 流程状态修改实体
+    /// </summary>
+    public class WorkFlowStatusChange
+    {
+        /// <summary>
+        /// 主键名称
+        /// </summary>
+        public string KeyName { get; set; }
+
+        /// <summary>
+        /// 主键值
+        /// </summary>
+        public string KeyValue { get; set; }
+
+        /// <summary>
+        /// 流程状态
+        /// </summary>
+        public WorkFlowStatus Status { get; set; }
+
+        /// <summary>
+        /// 表名称
+        /// </summary>
+        public string TableName { get; set; }
+
+        /// <summary>
+        /// 流程操作时间
+        /// </summary>
+        public long FlowTime { get; set; }
+
+        /// <summary>
+        /// CAP订阅名称
+        /// </summary>
+        public string TargetName { get; set; }
+    }
+}
