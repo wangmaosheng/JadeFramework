b7f4320f2a91cbadd7166ff0677d8f1534c7c295
wangmaosheng <wangmaosheng@51testing.com>
2021-06-09 17:34:48 +0800

    FlowAssign

---
+++ JadeFramework.WorkFlow/FlowAssign.cs
@@ -0,0 +1,23 @@
+﻿namespace JadeFramework.WorkFlow
+{
+    /// <summary>
+    /// 流程委托实体
+    /// </summary>
+    public class FlowAssign
+    {
+        /// <summary>
+        /// 委托人id
+        /// </summary>
+        public string AssignUserId { get; set; }
+
+        /// <summary>
+        /// 委托人姓名
+        /// </summary>
+        public string AssignUserName { get; set; }
+
+        /// <summary>
+        /// 委托信息
+        /// </summary>
+        public string AssignContent { get; set; }
+    }
+}
